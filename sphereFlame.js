50fd7b52d5e5d29c0182acc1a3abb47ca3d25a83
Mike Lynch <mike@mikelynch.org>
2019-05-06 20:33:05 +1000

    Real nice mistake (the crystal spheres one)

--- sphereFlame.js
+++ sphereFlame.js
@@ -1,15 +1,63 @@
+
+var sphereFrac;
+var drawer;
+
+var radius;
+var weight;
+
+
+function box(depth) {
+	push();
+	translate(0, 0, radius);
+	noStroke();
+	fill(255, 0, 0, 128);
+	box(weight * depth * 0.2);
+	pop();
+}
+
+function renderFrac(frac, depth, scale) {
+	drawer(depth, radius);
+	if( depth > 0 ) {
+		frac.forEach((trans) => {
+			push();
+			rotateX(scale * trans.twist);
+			rotateY(scale * trans.dip);
+			renderFrac(frac, depth - 1, trans.scale * scale);
+			pop();
+		})
+	}
+}
+
+
 function setup() {
 
 	createCanvas(windowWidth,windowHeight,WEBGL);
 
+	radius = 400;
+	weight = 10;
+
+	sphereFrac = [
+		{
+			dip: PI * .3,
+			twist: PI * .1,
+			scale: 0.8
+		},
+		{
+			dip: PI * -.2,
+			twist: PI * .15,
+			scale: 1.4
+		}
+	];
+
+	drawer = box;
 }
+
+
+
 
 function draw() {
 	background(255);
 
-	  box(30);
-  translate(100,100,-100);
-  rotate(PI/4, [1,1,0]);
-  box(30);
+	renderFrac(sphereFrac, 8, 1.0);
 
 }
